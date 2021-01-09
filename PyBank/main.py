b8a01616c89823cee389b9e24b614d136bd46089
diane yaneza <dianeyaneza@gmail.com>
2021-01-09 16:57:07 +0800

    Solved but missing average

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,54 +1,85 @@
 import os 
 import csv 
 
+# Import the csv file
 budget_csv = os.path.join('Resources', 'PyBank_Resources_budget_data.csv')
 
+# Set initial variables
+month_count = 0 
+total_profloss = 0
+
+max_inc = 0
+max_dec = 0
+max_inc_date = 0
+max_dec_date = 0
+
+total_change = 0
+prev_profloss = 0
+init_profloss = 0
+final_profloss = 0
+
+row_count = 0
+
+# Open and read the csv file to find the requirements 
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Read after the header row
     header = next(csvreader)
-       
-    # Set the base values for the parameters 
-    month_count = 0
-    total = 0
-    max_month = ""
-    min_month = ""
-    
-    # Read rows 
-    for row in csvreader:
-        # Set values for parameters
+
+    # Read rows         
+    for index, row in enumerate(csvreader):
+        
+        # Find the total number of months
         month_count += 1
-        total += int(row[1])
-        date = []
-        changes = []
+
+        # Find the total profit/loss
+        total_profloss += int(row[1])
+
+        # Set conditionals to determine the first and last values in profit and loss
+        if index == 0:
+            init_profloss = float(row[1])
+        elif index == row_count-1:
+            final_profloss = float(row[1])     
+
+        # Determine the change values in profit and loss from one to the next and so on
+        change = float(row[1]) - prev_profloss
+
+        # Determine the total change value from the sum of the values found
+        total_change += change
+
+        # Find the greatest increase in profits and loss from the changes
+        if change >= max_inc:
+            max_inc = int(change)  
+            # Identify the date of the greatest increase in the corresponding date column
+            max_inc_date = row[0]
+        # Find the greatest decrease in profits and loss from the changes
+        elif change <= max_dec:
+            max_dec = int(change) 
+            # Identify the date of the greatest decrease in the corresponding date column
+            max_dec_date = row[0]
+
+        # Save the updated previous profit and loss value
+        prev_profloss = float(row[1])
        
-    # Print Label
+        # Find the average change in profit and loss
+        ave_change = ((final_profloss - init_profloss) / (month_count - 1))
+
+    # Print Table Header
     print("Financial Analysis")
-    print("-------------------------")
+    print("---------------------------------------------------")
 
     # Print out the total months
     print(f"Total Months: {str(month_count)}")
     
     # Print out the total profits/loss
-    print(f"Total: ${str(total)}")
-
-    profit_loss = row[1]
-
-    # Loop through the rows to find the following info
-    for i in range(1, profit_loss):
-        changes = profit_loss[i+1] - profit_loss[i]
-        ave_change = sum(changes)/month_count
-        max_inc = max(changes)
-        max_inc_date = str(date[changes.index(max(changes))])
-        max_dec = min(changes)
-        max_inc_date = str(date[changes.index(min(changes))])
-
+    print(f"Total: ${str(total_profloss)}")
+        
     # Print out the average change
-    print(f"Average Change: ${str(ave_change)}")
+    # print(f"Average Change: ${str(ave_change)}")
     
-    # # Print out the greatest increase in profits month-year and $value
-    # print(f"Greatest Increase in Profits: {str(max_inc)}")
+    # Print out the greatest increase in profits month-year and $value
+    print(f"Greatest Increase in Profits: {str(max_inc_date)} (${str(max_inc)})")
     
-    # # Print out the greatest decrease in profits month-year and $value
-    # print(f"Greatest Decrease in Profits: {str(max_dec)}")
+    # Print out the greatest decrease in profits month-year and $value
+    print(f"Greatest Decrease in Profits: {str(max_dec_date)} (${str(max_dec)})")
